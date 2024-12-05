814335c5c01734b57c832326498ec1efd0cfa425
Raymonarch <138607857+Raymonarch@users.noreply.github.com>
2024-12-05 16:34:45 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve static HTML files from the "public" folder
-app.use(express.static(path.join(__dirname, "public")));
+// Middleware to parse incoming form data
+app.use(express.urlencoded({ extended: true }));
 
-// Define a route to serve the about page
-app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "about.html"));
+// Serve static HTML files from the "public" folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Home route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Form submission route
+app.post('/submit', (req, res) => {
+  const userName = req.body.name;
+  res.send(`<h1>Thank you for your submission, ${userName}!</h1>`);
 });
 
 // Start the server
